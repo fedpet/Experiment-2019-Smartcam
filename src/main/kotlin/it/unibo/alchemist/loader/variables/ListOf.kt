55d0dad24d861ae2251614d29a887e50d82382ed
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-10-20 12:06:58 +0200

    Add doc

--- src/main/kotlin/it/unibo/alchemist/loader/variables/ListOf.kt
+++ src/main/kotlin/it/unibo/alchemist/loader/variables/ListOf.kt
@@ -3,6 +3,9 @@ package it.unibo.alchemist.loader.variables
 import com.google.common.collect.Streams
 import java.util.stream.Stream
 
+/**
+ * A variable which can be any value contained in the specified list loaded via YAML.
+ */
 class ListOf<T : java.io.Serializable>(
     private val default: T,
     private vararg val values: T
