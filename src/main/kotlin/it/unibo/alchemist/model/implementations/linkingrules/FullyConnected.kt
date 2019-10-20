174dac3d7bad1d80a11db5337a3b0f568bc09976
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-10-20 12:24:59 +0200

    Add doc

--- src/main/kotlin/it/unibo/alchemist/model/implementations/linkingrules/FullyConnected.kt
+++ src/main/kotlin/it/unibo/alchemist/model/implementations/linkingrules/FullyConnected.kt
@@ -3,6 +3,9 @@ package it.unibo.alchemist.model.implementations.linkingrules
 import it.unibo.alchemist.model.implementations.neighborhoods.Neighborhoods
 import it.unibo.alchemist.model.interfaces.*
 
+/**
+ * Efficient [LinkingRule] for a fully connected network topology.
+ */
 class FullyConnected<T, P: Position<P>> : LinkingRule<T, P> {
     override fun computeNeighborhood(center: Node<T>, env: Environment<T, P>) =
         Neighborhoods.make(env, center, env.nodes.minusElement(center))
