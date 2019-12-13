21137240f976ea249b62dc34396d4f555110ea34
unknown <federico.pettinari2@studio.unibo.it>
2019-12-13 22:05:22 +0100

    Update doc

--- src/main/kotlin/it/unibo/smartcamexperiment/linpro/LinproCache.kt
+++ src/main/kotlin/it/unibo/smartcamexperiment/linpro/LinproCache.kt
@@ -1,16 +1,21 @@
 package it.unibo.smartcamexperiment.linpro
 
 import org.danilopianini.util.LinkedListSet
-import org.danilopianini.util.ListSet
 
 /**
  * Basic cache which takes into account only the last input.
  * Note: it does not take into consideration the cost function!
+ * See [CameraTargetAssignmentProblem] for a description of the parameters.
  */
 class LinproCache<S, D> {
     private var lastInput = Input<S, D>(emptySet(), emptySet(), 0, false) { _, _ -> 0.0 }
     private var lastResult = emptyMap<S, D>()
 
+    /**
+     * Get the cached result, or compute a new one if it is not present.
+     * The first parameters are those of [CameraTargetAssignmentProblem].
+     * @param calculate is the function which is called to calculate the correct result, in case the cache misses
+     */
     fun get(sources: List<S>,
             destinations: List<D>,
             maxSourcesPerDestination: Int,
@@ -24,13 +29,13 @@ class LinproCache<S, D> {
     ): Map<S, D> {
         val destsSet = LinkedListSet(destinations)
         val newInput = Input(sources.toSet(), destsSet, maxSourcesPerDestination, fair, cost)
-        if (lastInput != newInput) {
+        return if (lastInput != newInput) {
             lastInput = newInput
             lastResult = calculate(sources, destinations, maxSourcesPerDestination, fair, cost)
-            return lastResult
+            lastResult
         } else {
             // return updated positions
-            return lastResult.mapValues { entry -> destsSet[destsSet.indexOf(entry.value)] }
+            lastResult.mapValues { entry -> destsSet[destsSet.indexOf(entry.value)] }
         }
     }
 
