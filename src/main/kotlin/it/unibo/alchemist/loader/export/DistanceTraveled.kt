a116d14ceb28ed164652737f37e1d8150e662b28
Federico Pettinari <federico.pettinari2@studio.unibo.it>
2019-12-17 17:12:42 +0100

    Fix DistanceTraveled

--- src/main/kotlin/it/unibo/alchemist/loader/export/DistanceTraveled.kt
+++ src/main/kotlin/it/unibo/alchemist/loader/export/DistanceTraveled.kt
@@ -15,18 +15,21 @@ class DistanceTraveled<T, P : Position<P>> @JvmOverloads constructor(
     private val exportName: String = "distance",
     private val filterByMolecule: Molecule? = null
 ) : Extractor {
-    private val nodeToPosition = MapMaker().weakKeys().makeMap<Node<T>, P>()
+    // note: MapMaker makes ConcurrentMaps by default
+    private val lastPositionMap = MapMaker().weakKeys().makeMap<Environment<T, P>, MutableMap<Node<T>, P>>()
 
     override fun extractData(environment: Environment<*, *>, r: Reaction<*>?, time: Time?, step: Long): DoubleArray {
         @Suppress("UNCHECKED_CAST") val env = environment as Environment<T, P>
+        val newMap = mutableMapOf<Node<T>, P>()
+        val lastPositions = lastPositionMap.putIfAbsent(environment, newMap) ?: newMap
         var nodes: List<Node<T>> = env.nodes
         filterByMolecule?.also { molecule ->
             nodes = nodes.filter { it.contains(molecule) }
         }
-        return nodes.map { node ->
-            val currentPos = env.getPosition(node)
-            val distance = nodeToPosition[node]?.getDistanceTo(currentPos) ?: 0.0
-            nodeToPosition[node] = currentPos
+        return nodes.map {
+            val currentPos = env.getPosition(it)
+            val distance = lastPositions[it]?.getDistanceTo(currentPos) ?: 0.0
+            lastPositions[it] = currentPos
             distance
         }.sum().let { doubleArrayOf(it) }
     }
